c12e5a5f504e57e81850c62e7e71276b19f397af
Ashkan Kian <as627792@ucf.edu>
2025-03-07 18:06:00 -0500

    Created a Main World

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,7 +6,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("OpenWorld");
+        SceneManager.LoadScene("MainWorld");
     }
     public void Quit()
     {
